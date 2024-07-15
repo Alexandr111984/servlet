6e10d421d71b5fef961ff8325f315d2d6b09aef5
Alex <alexbatul@mail.ru>
2024-07-15 22:49:06 +0600

    change

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -4,12 +4,18 @@ import ru.netology.controller.PostController;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
 
+import javax.imageio.plugins.tiff.GeoTIFFTagSet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
+    private String path;
+    private String method;
     private PostController controller;
+    private static final String METHOD_GET = "GET";
+    private static final String METHOD_POST = "POST";
+    private static final String METHOD_DELETE = "DELETE";
 
     @Override
     public void init() {
@@ -22,24 +28,24 @@ public class MainServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
         // если деплоились в root context, то достаточно этого
         try {
-            final var path = req.getRequestURI();
-            final var method = req.getMethod();
+            path = req.getRequestURI();
+            method = req.getMethod();
             // primitive routing
-            if (method.equals("GET") && path.matches("/api/posts/?$")) {
+            if (method.equals(METHOD_GET) && path.matches("/api/posts/?$")) {
                 controller.all(resp);
                 return;
             }
-            if (method.equals("POST") && path.equals("/api/posts")) {
+            if (method.equals(METHOD_POST) && path.equals("/api/posts")) {
                 controller.save(req.getReader(), resp);
                 return;
             }
-            if (method.equals("DELETE") && path.matches("/api/posts/d+")) {
+            if (method.equals(METHOD_DELETE) && path.matches("/api/posts/d+")) {
                 // easy way
                 controller.removeById(getPostID(path), resp);
                 return;
             }
 
-            if (method.equals("GET") && path.matches("/api/posts/d+")) {
+            if (method.equals(METHOD_GET) && path.matches("/api/posts/d+")) {
                 // easy way
                 controller.getById(getPostID(path), resp);
                 return;
